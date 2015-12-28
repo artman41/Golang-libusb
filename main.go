0a3d77093e85043077703516c0e0191ac2840bcc
Seongmin Park <seongmi1@andrew.cmu.edu>
2015-12-27 21:22:25 -0500

    Started main
    Included the original libusb package, written in C.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import {
+    
+}
+
+type libusb_version struct {
+    major const uint16
+    minor const uint16
+    micro const uint16
+    nano const uint16
+    rc const string
+    describe const string
+}
+
+type libusb_context struct {
+    major const uint16
+    minor const uint16
+    micro const uint16
+    nano const uint16
+    rc const string
+    describe const string
+}
+
+
+func Libusb_init(ctx libusb_context**) int {
+    
+}
+
+func Libusb_set_debug(ctx libusb_context*. level int) {
+    
+}
+
+func Libusb_exit(ctx libusb_context*) {
+    
+}
